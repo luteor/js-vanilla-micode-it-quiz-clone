0aea5d41772a1c9fc6aacace51985095618497c8
leo-turco <leo.turco.83@gmail.com>
2023-11-09 21:34:39 +0100

    build: :construction: Creating quiz questions

---
+++ questions.js
@@ -0,0 +1,77 @@
+const questions = [
+  {
+    question: "Which company developed JavaScript?",
+    options: ["Microsoft", "Netscape", "Google"],
+    answer: "Netscape",
+  },
+  {
+    question: "When was the World Wide Web Consortium (W3C) founded?",
+    options: ["1994", "2000", "2002"],
+    answer: "1994",
+  },
+  {
+    question:
+      "What acronym refers to the markup language used to structure the content of web pages?",
+    options: ["HTML", "XML", "CSS"],
+    answer: "HTML",
+  },
+  {
+    question: "What is Git?",
+    options: [
+      "A version control system",
+      "A programming language",
+      "A network security protocol",
+    ],
+    answer: "A version control system",
+  },
+  {
+    question:
+      "What is the main function of the HTTPS protocol on the internet?",
+    options: ["Encrypt data", "Speed up browsing", "Manage emails"],
+    answer: "Encrypt data",
+  },
+  {
+    question: "What is a search engine?",
+    options: [
+      "Software for browsing the web",
+      "A tool for searching for information on the internet",
+      "A hardware component storing data online",
+    ],
+    answer: "A tool for searching for information on the internet",
+  },
+  {
+    question: "In which year was the Google Chrome browser launched?",
+    options: ["2008", "2010", "2012"],
+    answer: "2008",
+  },
+  {
+    question:
+      "What does the term 'open source' represent in the context of software?",
+    options: [
+      "Free software",
+      "Software whose source code is available and can be modified",
+      "Proprietary software",
+    ],
+    answer: "Software whose source code is available and can be modified",
+  },
+  {
+    question: "What is the main goal of a VPN?",
+    options: [
+      "Speed up the connection",
+      "Ensure the confidentiality of data transmitted over a public network",
+      "Block access to certain websites",
+    ],
+    answer:
+      "Ensure the confidentiality of data transmitted over a public network",
+  },
+  {
+    question: "What is a brute force attack?",
+    options: [
+      "An attack that uses viruses",
+      "An attack aimed at guessing a password by trying different combinations",
+      "An attack by flooding requests on a server",
+    ],
+    answer:
+      "An attack aimed at guessing a password by trying different combinations",
+  },
+];
